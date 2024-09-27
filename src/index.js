3d87f80de22f0f3c7bbb73d6024935aa226db237
weebo-ary <aryan.bhardwaj1233@gmail.com>
2024-09-28 01:47:53 +0530

    removed app.js from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import MainRoutes from './routes/MainRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
