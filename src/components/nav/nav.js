09591b912f6df83b1c273c7ab1a0d85e49d3a7da
weebo-ary <aryan.bhardwaj1233@gmail.com>
2024-10-03 13:57:08 +0530

    updated Nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,7 +4,7 @@ import DropdownMenu from "../dropdown/DropdownMenu";
 import DropdownMenuProducts from "../dropdown/DropDownProducts";
 import DarkLogo from "../../assets/Logo/DarkLogo.png";
 import LightLogo from "../../assets/Logo/LightLogo.png";
-import { MenuOutlined } from "@ant-design/icons";
+import { LoginOutlined, MenuOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
 import "./nav.css";
 import Sidebar from "../Drawer/Sidebar";
@@ -36,7 +36,7 @@ const Navbar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
-   useEffect(() => {
+  useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
         setIsScrolled(true);
@@ -100,16 +100,18 @@ const Navbar = () => {
             onMouseEnter={() => setDropdownProduct(true)}
             onMouseLeave={() => setDropdownProduct(false)}
           >
-            <DotChartOutlined /> <span style={{ cursor: "pointer" }}>Products</span>
+            <DotChartOutlined />{" "}
+            <span style={{ cursor: "pointer" }}>Products</span>
             {dropdownOpenProduct && <DropdownMenuProducts />}
           </div>
-          
+
           <div
             className="relative text-sm font-medium p-1 text-gray-700 dark:text-gray-300 hover:text-gray-500 dark:hover:text-indigo-400"
             onMouseEnter={() => setDropdownOpen(true)}
             onMouseLeave={() => setDropdownOpen(false)}
           >
-            <DotChartOutlined /> <span style={{ cursor: "pointer" }}>Solutions</span>
+            <DotChartOutlined />{" "}
+            <span style={{ cursor: "pointer" }}>Solutions</span>
             {dropdownOpen && <DropdownMenu />}
           </div>
           <div
@@ -117,7 +119,8 @@ const Navbar = () => {
             onMouseEnter={() => setDropdownServices(true)}
             onMouseLeave={() => setDropdownServices(false)}
           >
-            <ControlOutlined /> <span style={{ cursor: "pointer" }}>Services</span>
+            <ControlOutlined />{" "}
+            <span style={{ cursor: "pointer" }}>Services</span>
             {dropdownOpenServices && <DropdownMenuServices />}
           </div>
           <NavLink
@@ -128,11 +131,11 @@ const Navbar = () => {
                 : "relative text-sm p-1 font-medium text-gray-700 dark:text-gray-300 marker-underline hover:text-gray-500 dark:hover:text-indigo-400"
             }
           >
-           <PhoneOutlined /> Contact
+            <PhoneOutlined /> Contact
           </NavLink>
         </div>
       </div>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 md:space-x-4 sm:space-x-3 xs:space-x-3">
         <div
           className="md:hidden xs:flex p-2 rounded-md bg-transparent hover:text-gray-500 dark:hover:text-gray-300 text-black dark:text-white transition duration-500"
           onClick={toggleSidebar}
@@ -145,7 +148,11 @@ const Navbar = () => {
           alt="User Avatar"
           className="h-5 w-5 rounded-full"
         />
+        <button className="flex items-center justify-center p-3 md:p-5 md:py-2 sm:p-2 xs:p-2 md:ml-0 xs:ml-5 bg-gray-800 hover:bg-gray-600 dark:hover:bg-gray-900 dark:bg-gray-700 text-white rounded-lg">
+          <span className="md:text-xs sm:text-xs xs:text-xs">Login <LoginOutlined style={{marginLeft:'5px'}}/></span> 
+        </button>
       </div>
+
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
     </nav>
   );
