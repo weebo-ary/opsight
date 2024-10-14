b1bc4fc334f50fb4feca079c61d5af2e78811f10
weebo-ary <aryan.bhardwaj1233@gmail.com>
2024-10-14 11:24:26 +0530

    update

--- src/pages/products/Products/banner.js
+++ src/pages/products/Products/banner.js
@@ -82,7 +82,7 @@ const BannerWithFadingGridBackground = () => {
           for your Factory Digitization
             </h1>
         <p className="text-base md:text-lg text-gray-500 dark:text-gray-400 mb-10">
-        Unlock the full potential of your industrial assets with OpsightAI's IIoT Gateway. Our gateway seamlessly connects your machines, sensors, and devices, providing a reliable and flexible platform for real-time data acquisition and processing.
+        Optimize Production through OpsightAI’s integrated AIoT platform which seamlessly integrates your shop-floor assets.
         </p>
 
         {/* <div className="flex flex-col md:flex-row sm:flex-row xs:flex-row justify-start md:justify-center space-y-4 md:space-y-0 md:space-x-4">
